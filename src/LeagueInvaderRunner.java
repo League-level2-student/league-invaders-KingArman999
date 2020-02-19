4a83d090ddd3f8f7f2d141355927c57366896e8e
League Student <league@iMac-38.attlocal.net>
2020-02-18 19:58:18 -0800

    LeagueInvaderPART1notTEXT

---
+++ src/LeagueInvaderRunner.java
@@ -0,0 +1,7 @@
+
+public class LeagueInvaderRunner {
+	public static void main(String[] args) {
+		LeagueInvaders l = new LeagueInvaders();
+		l.setup();
+	}
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,20 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	GamePanel game;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
 
+	LeagueInvaders() {
+		frame = new JFrame();
+		frame.setVisible(true);
+		game = new GamePanel();
+	}
+
+	void setup() {
+		frame.add(game);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
